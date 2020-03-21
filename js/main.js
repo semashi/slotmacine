675dab6383ffb7611192f879ab1ec41207df7a82
小野博司 <onohiroshi@mbp-3.local>
2020-03-21 15:54:43 +0900

    add slotmacine to ms=>spin()

---
+++ js/main.js
@@ -0,0 +1,48 @@
+'use strict';
+
+{
+  class Panel {
+    constructor() {
+      const section = document.createElement('section');
+      section.classList.add('panel');
+
+      
+      this.img = document.createElement('img');
+      this.img.src = 'img/seven.png';
+      
+      this.stop = document.createElement('div');
+      this.stop.textContent = 'STOP';
+      this.stop.classList.add('stop');
+      
+      section.appendChild(this.img);
+      section.appendChild(this.stop);
+      
+      const main = document.querySelector('main');
+      main.appendChild(section);
+
+      // console.log(section);
+    }
+
+    spin(){
+      
+    }
+
+    }
+
+
+  }
+
+  const panels = [
+    new Panel(),
+    new Panel(),
+    new Panel(),
+  ];
+
+  const spin = document.getElementById('spin');
+  spin.addEventListener('click', () => {
+    panels.forEach(panel => {
+      panel.spin();
+    });
+  });
+
+}
